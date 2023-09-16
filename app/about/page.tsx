350cd48ff816f6a19aa735244f13ccaf8b0610fb
Hassan <github/HassanFarooq704>
2023-09-16 22:38:26 +0500

    first commit

---
+++ app/about/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function about() {
+    return (
+        <>
+            <h1>About Page</h1>
+            <br />
+            <Image src="/img.jpeg" alt="About Image" width={"300"} height={"300"} />
+            <br />
+            <Link href={"/"}>Home</Link>
+        </>
+    )
+}
