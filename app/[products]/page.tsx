3bc7bb7c60881915f23aaa02d31655097d018868
Hassan <github/HassanFarooq704>
2023-09-18 17:24:47 +0500

    continue

--- app/[products]/page.tsx
+++ app/[products]/page.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link"
 
-export default function Home() {
+export default function Home({params}:{params:{products:string}}) {
   return (
     <>
       <h1>Home</h1>
       <Link href="/about">About Page</Link>
+      <div>Your product name is:{params.products}</div>
     </>
   )
 }
